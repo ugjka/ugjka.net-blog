12bdb3d94006eb2cafaf6f1061df6b9f7260042c
Uģis Ģērmanis <ugis@germanis.lv>
2025-02-02 19:16:54 +0200

    stuff

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-bundle exec jekyll build
+bundle-2.7 exec jekyll build
 find _site \( -name '*.css' -o -name '*.html' -o -name '*.js' -o -name '*.json' -o -name '*.txt' -o -name '*.xml' -o -name '*.yml' \) -exec gzip --best --keep -f {} \;
-rsync -Pvhr --delete _site/ ugjka.net:share/blog/
+rsync -Pvhr --delete _site/ server.internal:share/blog/
