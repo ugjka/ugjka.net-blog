fc37b04adc2a1a8b71268a1d14a470f17b483796
Uģis Ģērmanis <ugis@germanis.lv>
2025-10-10 04:14:32 +0300

    up

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-bundle-2.7 exec jekyll build
+set -e
+nix shell github:bobvanderlinden/nixpkgs-ruby#'"ruby-2.7"' --extra-experimental-features nix-command --extra-experimental-features flakes --command \
+bundle exec jekyll build
 find _site \( -name '*.css' -o -name '*.html' -o -name '*.js' -o -name '*.json' -o -name '*.txt' -o -name '*.xml' -o -name '*.yml' \) -exec gzip --best --keep -f {} \;
 rsync -Pvhr --delete _site/ server.internal:share/blog/
