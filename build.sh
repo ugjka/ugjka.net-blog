09f5474227571f10cb13708ae7c3da7944b40b96
Uģis Ģērmanis <ugis@germanis.lv>
2026-02-08 23:47:22 +0200

    nu

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
-nix shell github:bobvanderlinden/nixpkgs-ruby#'"ruby-2.7"' --extra-experimental-features nix-command --extra-experimental-features flakes --command \
-bundle install --deployment
-nix shell github:bobvanderlinden/nixpkgs-ruby#'"ruby-2.7"' --extra-experimental-features nix-command --extra-experimental-features flakes --command \
-bundle exec jekyll build
+bundle.ruby2.7 config set --local deployment 'true'
+
+bundle.ruby2.7 install
+
+bundle.ruby2.7 exec jekyll build
+
 find _site \( -name '*.css' -o -name '*.html' -o -name '*.js' -o -name '*.json' -o -name '*.txt' -o -name '*.xml' -o -name '*.yml' \) -exec gzip --best --keep -f {} \;
 rsync -Pvhr --delete _site/ server.internal:share/blog/
